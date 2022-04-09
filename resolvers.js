10004b3d7ccc2c0e98aecdc480802b1dc028378a
Benny Chan <bennyx1993@gmail.com>
2022-04-09 15:49:55 -0500

    nesting data - speakers into sessions

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 module.exports = {
   Query: {
     sessions: (parent, args, { dataSources }, info) => {
@@ -13,4 +14,14 @@ module.exports = {
       return dataSources.speakerAPI.getSpeakerById(id);
     }
   },
+  Session: {
+    async speakers(session, args, { dataSources }, info) {
+      const speakers = await dataSources.speakerAPI.getSpeakers();
+      const returns = speakers.filter((speaker) => {
+        return _.filter(session.speakers, { id: speaker.id }).length > 0;
+      });
+      
+      return returns;
+    }
+  }
 };
